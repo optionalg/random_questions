97bcd4fdb53ea89f43a93e885231bcb2d8282362
qdonnellan <qdonnellan@gmail.com>
2014-01-03 11:34:49 -0600

    added a second solution to the comparing integers without comparison problem

--- comparing_integers.py
+++ comparing_integers.py
@@ -1,7 +1,11 @@
 # Write a function that compares two integers without using a comparison operator
 
 # Solution 1: Using a dict and try/except
-def compare_ints(a, b):
+def compare_ints_1(a, b):
+  '''
+  checks if two integers, a and b, are equal by dividing them and forcing a
+  dict lookup of the result which will fail if it is anything other than 1
+  '''
   d = {1.0 : True}
   try:
     c = float(a)/float(b)
@@ -16,4 +20,15 @@ def compare_ints(a, b):
   except:
     return False
 
-print compare_ints(0,0)
+# Solution 2: Catching a division by 0 error as confirmation of equality
+def compare_ints_2(a,b):
+  '''
+  checks if two integers, a and b, are equal by subtracting them then dividing
+  an arbitrary integer by the result. If they are equal it should raise a 
+  division by zero error
+  '''
+  try:
+    d = 1.0/(a-b)
+    return False
+  except:
+    return True
--- comparing_integers_test.py
+++ comparing_integers_test.py
@@ -1,16 +1,23 @@
 import unittest
-from comparing_integers import compare_ints
+from comparing_integers import compare_ints_1, compare_ints_2
 
 class test_compare_ints_function(unittest.TestCase):
 
   def test_equal_integers(self):
     '''make sure that two equal integers are found to be equal'''
-    self.assertEqual(compare_ints(2,2), True)
+    self.assertTrue(compare_ints_1(2,2))
+    self.assertTrue(compare_ints_2(2,2))
 
   def test_unequal_integerts(self):
     '''make sure that two unequal integers are found to be unequal'''
-    self.assertEqual(compare_ints(2,3), False)
+    self.assertFalse(compare_ints_1(2,3))
+    self.assertFalse(compare_ints_2(2,3))
 
   def test_zero_comparison(self):
     '''make sure that a zero as the second integer doesnt break the function'''
-    self.assertEqual(compare_ints(2,0), False)
+    self.assertFalse(compare_ints_1(2,0))
+    self.assertFalse(compare_ints_2(2,0))
+
+  def test_both_zeros_comparison(self):
+    self.assertTrue(compare_ints_1(0,0))
+    self.assertTrue(compare_ints_2(0,0))
