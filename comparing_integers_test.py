6a49a5ee9ed4664cb41c164f0d34c350c0f4ff8f
qdonnellan <qdonnellan@gmail.com>
2014-01-03 11:20:37 -0600

    added some docstrings

--- comparing_integers_test.py
+++ comparing_integers_test.py
@@ -4,10 +4,13 @@ from comparing_integers import compare_ints
 class test_compare_ints_function(unittest.TestCase):
 
   def test_equal_integers(self):
+    '''make sure that two equal integers are found to be equal'''
     self.assertEqual(compare_ints(2,2), True)
 
   def test_unequal_integerts(self):
+    '''make sure that two unequal integers are found to be unequal'''
     self.assertEqual(compare_ints(2,3), False)
 
-  def test_zero_comparision(self):
+  def test_zero_comparison(self):
+    '''make sure that a zero as the second integer doesnt break the function'''
     self.assertEqual(compare_ints(2,0), False)
