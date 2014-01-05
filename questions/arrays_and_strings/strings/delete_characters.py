bff120328fbc80909924b5d167e8d499b5314413
qdonnellan <qdonnellan@gmail.com>
2014-01-05 15:19:28 -0600

    changes references to “word” to “string”

--- questions/arrays_and_strings/strings/delete_characters.py
+++ questions/arrays_and_strings/strings/delete_characters.py
@@ -1,7 +1,7 @@
-def delete_characters(word, to_delete):
+def delete_characters(string, letters_to_delete):
   '''
-  given a word delete each instance of the characters given to_delete
+  given a string, delete each instance of the characters given letters_to_delete
   '''
-  for letter in to_delete:
-    word = word.replace(letter, '')
-  return word
+  for letter in letters_to_delete:
+    string = string.replace(letter, '')
+  return string
