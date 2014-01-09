ad6378d69563128c4da149b005e6bd91ce2ead1a
qdonnellan <qdonnellan@gmail.com>
2014-01-08 19:10:15 -0600

    fixed typo in docstring

--- questions/recursion/permutations_test.py
+++ questions/recursion/permutations_test.py
@@ -5,7 +5,7 @@ class test_permutations_implementation(unittest.TestCase):
 
   def test_three_word_case(self):
     '''
-    test that when passed HAT the function returns all permutations of this
+    test that when passed HAT the function returns all permutations of HAT (in correct order)
     '''
     string = 'HAT'
     expected = ['HAT', 'HTA', 'AHT', 'ATH', 'THA', 'TAH']
