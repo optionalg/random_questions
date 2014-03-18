29162bec3f15a22ea4a35525d200c854a78214dd
qdonnellan <qdonnellan@gmail.com>
2014-03-18 10:41:31 -0500

    added a simple case

--- questions/recursion/telephone_word_combos_test.py
+++ questions/recursion/telephone_word_combos_test.py
@@ -6,13 +6,21 @@ class TelephoneWordCombosTest(unittest.TestCase):
     test the implementation of telephone_word_combos
     """
 
+    def test_case_with_three_numbers(self):
+        """
+        test the case with the number 111
+        there is only one combination: '111'
+        """
+        result = telephone_word_combos('111', combos=[])
+        self.assertEqual(result,['111'])
+
     def test_case_with_no_ones_or_zeros(self):
         """
         test the case with the number 111-1111
 
         should yield one combination: '1111111'
         """
-        result = telephone_word_combos('1111111')
+        result = telephone_word_combos('1111111', combos=[])
         self.assertEqual(result, ['1111111'])
 
     def test_case_with_only_one_mutable(self):
@@ -21,7 +29,7 @@ class TelephoneWordCombosTest(unittest.TestCase):
 
         should yeild '111111[ABC]'
         """
-        result = telephone_word_combos('1111112')
+        result = telephone_word_combos('1111112', combos=[])
         self.assertEqual(len(result), 3)
         self.assertIn('111111A', result)
         self.assertIn('111111B', result)
@@ -33,7 +41,7 @@ class TelephoneWordCombosTest(unittest.TestCase):
 
         should yeild '11111[ABC][DEF]', 9 combinations
         """
-        result = telephone_word_combos('1111123')
+        result = telephone_word_combos('1111123', combos=[])
         self.assertEqual(len(result), 9)
 
     def test_case_with_max_number_mutables(self):
@@ -42,5 +50,5 @@ class TelephoneWordCombosTest(unittest.TestCase):
 
         should yeild a result with 3**7 combinations
         """
-        result = telephone_word_combos('2222222')
+        result = telephone_word_combos('2222222', combos=[])
         self.assertEqual(len(result), 3**7)
